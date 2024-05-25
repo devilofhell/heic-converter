fe01a8cde81906c0898d38b63caa9ac4bace997e
Justin Stieler <justin.raeder@stud.tu-darmstadt.de>
2024-05-25 14:47:17 +0200

    added support for file-ownership
    Co-authored-by: genesispc <marcin.metter@outlook.com>

--- main.go
+++ main.go
@@ -7,8 +7,10 @@ import (
 	"log"
 	"os"
 	"os/exec"
+	"os/user"
 	"path"
 	"path/filepath"
+	"strconv"
 	"strings"
 	"time"
 )
@@ -17,6 +19,7 @@ var watchingFolders string
 var timeBetweenConverting string
 var keepOriginals string
 var keepLivePhoto string
+var username string
 
 func main() {
 	ReadEnv()
@@ -49,25 +52,11 @@ func Run(folder string) {
 	}
 }
 
-// func ConvertFolder(folder string) error {
-// 	//files, _ := ioutil.ReadDir(folder)
-
-// 	filepath.WalkDir(folder, Convert)
-
-// 	return nil
-// }
-
 func ConvertHeic(path string, srcFileName string, d fs.DirEntry, err error) error {
 	if d.IsDir() {
 		return nil
 	}
 	newFileName := fmt.Sprintf("%s.jpg", srcFileName)
-
-	// fmt.Printf("CurrentFile: %s\n", filepath.Join(path, d.Name()))
-	// fmt.Printf("NewFile    : %s\n\n", filepath.Join(path, newFileName))
-
-	// fmt.Printf("splitPath  : %v\n", currentFolder)
-	// fmt.Printf("cmdDir     : %s\n", path)
 
 	cmd := exec.Command("convert")
 	cmd.Dir = path
@@ -78,7 +67,7 @@ func ConvertHeic(path string, srcFileName string, d fs.DirEntry, err error) erro
 	go io.Copy(os.Stdout, stdOut)
 	cerr = cmd.Run()
 	if cerr != nil {
-		fmt.Printf("ERROR: %v\n", cerr)
+		log.Printf("ERROR: %v\n", cerr)
 		return cerr
 	}
 	log.Printf("successful converting of [%s  ->  %s]\n", d.Name(), newFileName)
@@ -90,6 +79,7 @@ func ReadEnv() {
 	timeBetweenConverting = os.Getenv("TIME_BETWEEN")
 	keepOriginals = os.Getenv("KEEP_ORIGINAL")
 	keepLivePhoto = os.Getenv("KEEP_LIVE_PHOTO")
+	username = os.Getenv("USERNAME")
 
 	if watchingFolders == "" {
 		log.Fatalf("no folders to watch specified. set the WATCH environment variable. quit programm")
@@ -102,12 +92,15 @@ func ReadEnv() {
 	}
 	log.Printf("start converting every: %s", timeBetweenConverting)
 	if keepOriginals == "" {
-		keepOriginals = "false"
-		log.Printf("KEEP_ORIGNAL not specified. setting default to false")
+		keepOriginals = "true"
+		log.Printf("KEEP_ORIGNAL not specified. setting default to true")
 	}
 	if keepLivePhoto == "" {
-		keepLivePhoto = "false"
-		log.Printf("KEEP_LIVE_PHOTO not specified. setting default to false")
+		keepLivePhoto = "true"
+		log.Printf("KEEP_LIVE_PHOTO not specified. setting default to true")
+	}
+	if username == "" {
+		log.Printf("USERNAME not specified. setting default to 'no change'")
 	}
 }
 
@@ -135,6 +128,10 @@ func WalkDeleteHeic(folder string) error {
 				continue
 			}
 
+			if username != "" {
+				UpdateFileOwner(folder, name)
+			}
+
 			if keepLivePhoto == "false" {
 				DeleteLivePhoto(moduleFolder, folder, name)
 			}
@@ -147,6 +144,30 @@ func WalkDeleteHeic(folder string) error {
 		}
 	}
 	return nil
+}
+
+func UpdateFileOwner(path string, srcFileName string) {
+	// Get the userid and groupid from the username
+	var userID, groupID int
+	systemUser, err := user.Lookup(username)
+	if err != nil {
+		log.Printf("Error: %s \n", err)
+		userID = -1
+		groupID = -1
+	} else {
+		userID, _ = strconv.Atoi(systemUser.Uid)
+		groupID, err = strconv.Atoi(systemUser.Gid)
+		if err != nil {
+			log.Printf("Error: %s \n", err)
+			groupID = -1
+		}
+	}
+	newFile := filepath.Join(path, srcFileName + ".jpg")
+	if err := os.Chown(newFile, userID, groupID); err != nil {
+		log.Printf("Error: %s \n", err)
+		return
+	}
+	log.Printf("Updated %s: UID=%v, GUID=%v \n", newFile, userID, groupID)
 }
 
 func IsAlreadyConverted(moduleFolder []fs.DirEntry, fileName string) bool {
